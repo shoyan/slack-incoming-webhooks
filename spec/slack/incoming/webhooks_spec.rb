2055825bee231cfa64bde92c04265313ec99c749
Shohei Yamasaki <s-yamasaki@pepabo.com>
2015-09-25 17:37:35 +0900

    Add test

--- spec/slack/incoming/webhooks_spec.rb
+++ spec/slack/incoming/webhooks_spec.rb
@@ -4,4 +4,38 @@ describe Slack::Incoming::Webhooks do
   it 'has a version number' do
     expect(Slack::Incoming::Webhooks::VERSION).not_to be nil
   end
+
+  subject { described_class.new 'http://example.com' }
+
+  describe "#initialize" do
+    it "sets the given webhook_url" do
+      expect( subject.webhook_url).to eq 'http://example.com'
+    end
+
+    it "sets the payload options" do
+      subject = described_class.new 'http://example.com', channel: 'foo'
+      expect( subject.payload).to eq channel: 'foo'
+    end
+  end
+
+  describe "#channel=" do
+    it "sets the given channel" do
+      subject.channel = "#foo"
+      expect( subject.channel ).to eq "#foo"
+    end
+  end
+
+  describe "#username=" do
+    it "sets the given username" do
+      subject.username = "foo"
+      expect( subject.username ).to eq "foo"
+    end
+  end
+
+  describe "#attachments=" do
+    it "sets the given attachments" do
+      subject.attachments= { color: "good", text: "sample" }
+      expect( subject.attachments ).to eq [{color: "good", text: "sample"}]
+    end
+  end
 end
